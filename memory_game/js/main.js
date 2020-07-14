047f1596952c18fe9d5deff6eff11c8faa0ecbf7
MalishaShah <41448279+MalishaShah@users.noreply.github.com>
2020-07-14 16:24:07 -0400

    Added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,12 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2){
+    let message = cardsInPlay[0] === cardsInPlay[1]? "You found a match!" : "Sorry, try again.";
+    alert(message);
+}
