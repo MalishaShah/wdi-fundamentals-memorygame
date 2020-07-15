921d8aa50898a39495e2563ffa50acca70dabc20
MalishaShah <41448279+MalishaShah@users.noreply.github.com>
2020-07-14 21:09:36 -0400

    Added interactive features to click and flip the image of the card

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,52 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if(cardsInPlay.length === 2){
-    let message = cardsInPlay[0] === cardsInPlay[1]? "You found a match!" : "Sorry, try again.";
-    alert(message);
+
+function checkForMatch(){
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
 }
+
+function flipCard(){
+    let cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId]);
+    if(cardsInPlay.length === 2){
+        checkForMatch();
+    }
+}
+function createBoard (){
+    for(var i = 0; i<cards.length; i++){
+        var newCard = document.createElement('img');
+        newCard.setAttribute('src', 'images/back.png');
+        newCard.setAttribute('data-id', i);
+        newCard.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(newCard);
+    }
+}
+createBoard();
